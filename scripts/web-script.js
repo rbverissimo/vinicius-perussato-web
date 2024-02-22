9a84764590b2d85dba5b5580d95c60afe9031fb7
rbverissimo <rverissimo.dev@gmail.com>
2024-02-22 06:54:00 -0300

    Ajustes finais no projeto comgás

--- scripts/web-script.js
+++ scripts/web-script.js
@@ -8,33 +8,3 @@ document.getElementById('idioma-select').addEventListener('change', function() {
       window.location.href = idiomaSelecionado;
     }
   });
-
-
-  document.getElementById('scroll-to-footer').addEventListener('click', function(event){
-    event.preventDefault();
-
-    const target = document.getElementById('target-footer');
-    const posicaoTarget = target.getBoundingClientRect().top + window.scrollY;
-    const posicaoInicial = window.scrollY;
-
-    const distanciaScroll = posicaoTarget - posicaoInicial;
-    const duracao = 1000;
-    let inicioTempo = null;
-
-    function scrollAnimation(tempo) {
-        if (inicioTempo === null) inicioTempo = tempo;
-        var tempoCorrido = tempo - inicioTempo;
-        var scrollAmount = easeInOutQuad(tempoCorrido, posicaoInicial, distanciaScroll, duracao);
-        window.scrollTo(0, scrollAmount);
-        if (tempoCorrido < duracao) requestAnimationFrame(scrollAnimation);
-    }
-
-    function easeInOutQuad(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
-    }
-
-    requestAnimationFrame(scrollAnimation);
-  });
